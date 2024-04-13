52d903ee88850a0ea3d7813ce28e2a9a39cef7d2
IsmaelCardoso <ismael.cardosos@gmail.com>
2024-04-13 20:51:03 -0300

    feat: Created product-table

--- frontend-angular/src/app/components/product/product-create/product-create.component.ts
+++ frontend-angular/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -8,6 +9,12 @@ import { Router } from '@angular/router';
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: 0,
+  }
+
   constructor(
     private productService: ProductService,
     private router: Router
@@ -18,7 +25,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Operação realizada com sucesso')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Operação realizada com sucesso')
+      this.router.navigate(['/products'])
+    })
+
   }
 
   cancel(): void {
---
+++ frontend-angular/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrl: './product-read.component.css'
+})
+export class ProductReadComponent implements OnInit {
+
+  products: Product[] = [{
+    id: 0,
+    name: '',
+    price: 0,
+  }]
+  displayedColumns: string[] = ['id', 'name', 'price']
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.productService.read().subscribe((products) => {
+      this.products = products
+
+      console.log(this.products)
+    })
+  }
+}
